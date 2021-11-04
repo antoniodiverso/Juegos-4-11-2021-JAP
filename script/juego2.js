a9f7c6801de8412112fd17e4fc195b118fdb110d
antoniodiverso <77039172+antoniodiverso@users.noreply.github.com>
2021-11-04 09:44:54 -0300

    Adivinar Numero terminado

---
+++ script/juego2.js
@@ -0,0 +1,42 @@
+let numeroRandom;
+let vidas = 10;
+
+document.addEventListener("DOMContentLoaded", function (e) {
+
+    numeroRandom = Math.floor(Math.random() * 100);
+
+
+});
+
+
+
+function checkearNumero() {
+
+    let inputNumero = document.getElementById('inputNumero');
+    let adivinaste = document.getElementById('adivinaste');
+
+    console.log(inputNumero.value)
+
+    if (inputNumero.value != "") {
+
+        
+        if (vidas > 0) {
+            if (inputNumero.value > numeroRandom) {
+                alert('El número random es más chico');
+            }
+            else if (inputNumero.value < numeroRandom) {
+                alert('El número random es más grande');
+            } 
+            else {
+                alert("VAMO LOS PIBES!!");
+                window.location = "juego2.html";
+            }
+            
+        } else {
+            alert("Te quedaste sin vidas capo");
+            window.location = "juego2.html";
+        }
+        vidas--;
+        console.log(vidas);
+    }
+}
