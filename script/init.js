dd9840e017b43d9dc96b5b75f28b3bd3cd4390dc
antoniodiverso <77039172+antoniodiverso@users.noreply.github.com>
2021-11-11 10:03:38 -0300

    Agregué los textos abajo de los inputs de usuario y contrasena

--- script/init.js
+++ script/init.js
@@ -1,64 +1,70 @@
 let UsuariosURL = "https://danikho2020.github.io/json/user.json";
 let users = [];
 
-var getJSONData = function(url){
+var getJSONData = function (url) {
     var result = {};
 
     return fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }else{
-        throw Error(response.statusText);
-      }
-    })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-         
-          return result;
-    })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw Error(response.statusText);
+            }
+        })
+        .then(function (response) {
+            result.status = 'ok';
+            result.data = response;
 
-        return result;
-    });
+            return result;
+        })
+        .catch(function (error) {
+            result.status = 'error';
+            result.data = error;
+
+            return result;
+        });
 }
 
 
 document.addEventListener("DOMContentLoaded", function (e) {
 
-    getJSONData(UsuariosURL).then(function(resultObj){
-        if (resultObj.status === "ok"){
-           users.push (resultObj.data[0]);
+    getJSONData(UsuariosURL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            users.push(resultObj.data[0]);
         }
     });
 
 
 });
 
-document.getElementById("singin").addEventListener("click" ,function(event){
+document.getElementById("singin").addEventListener("click", function (event) {
     event.preventDefault();
     let usuarioIn = document.getElementById("inputEmail");
     let contraseñaIn = document.getElementById("inputPassword");
 
+
     users.forEach(usuario => {
-        if (usuario.user == usuarioIn.value){
+        if (usuario.user == usuarioIn.value) {
             usuarioIn.classList.remove("alert-danger")
-            if (usuario.password == contraseñaIn.value){
+            document.getElementById("emailIncorrecto").innerHTML = '';
+            
+            if (usuario.password == contraseñaIn.value) {
                 contraseñaIn.classList.remove("alert-danger")
-                alert("jaja")
+                document.getElementById("passwordIncorrecta").innerHTML = '';
+                alert("Ingresado con exito!")
             }
-            else{
+            else {
+                document.getElementById("passwordIncorrecta").innerHTML = '';
                 contraseñaIn.classList.add("alert-danger")
-                usuarioIn.placeholder = 'Contraseña Incorrecto';
+                document.getElementById("passwordIncorrecta").innerHTML += 'Contraseña Incorrecta';
             }
-        }else{
+        } else {
+            document.getElementById("emailIncorrecto").innerHTML = '';
             usuarioIn.classList.add("alert-danger")
-            usuarioIn.placeholder = 'Usuario Incorrecto';
+            document.getElementById("emailIncorrecto").innerHTML += 'Usuario Incorrecto';
         }
-        
+
     });
 
 
