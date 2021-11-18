af19f4e1d8688c1ea7fc5caec3c608b249ccea37
antoniodiverso <77039172+antoniodiverso@users.noreply.github.com>
2021-11-18 09:37:17 -0300

    Ahí andan los html desde el server

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const loginJson = require("./usuarios.json")
 const app = express();
 const port = 3000;
+app.use(express.static(__dirname + "/static"));
 
 app.get("/", (req, res) => {
-    res.send("Funciona el server!")
+    res.send()
 
 })
+
+app.get("/usuarios", (req, res) => {
+    res.send(loginJson)
+
+})
+
+
 
 app.listen(port, () => {
     console.log("Escuchando a http://localhost:" + port)
